6e5dc3cddb762a20594f4c8bb397b0d7ee6453fa
Aseel <aseelarr@gmail.com>
2025-05-12 20:21:58 +0000

    update the files

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+# Exit if KEY_PATH is not set
 if [ -z "$KEY_PATH" ]; then
   echo "KEY_PATH env var is expected"
   exit 5
 fi
 
-if [ -z "$1" ]; then
+
+# Get the number of arguments
+ARGC=$#
+
+
+
+# Case 2: Only Bastion IP provided (connect to bastion)
+if [ $ARGC -eq 1 ]; then
+  BASTION_IP=$1
+  ssh -i "$KEY_PATH" ubuntu@$BASTION_IP
+
+# Case 1 and 3: Bastion IP + Target private IP + optional command
+elif [ $ARGC -ge 2 ]; then
+  BASTION_IP=$1
+  TARGET_IP=$2
+  shift 2
+
+  # If a command is provided (Case 3), run it on the target
+  if [ $# -gt 0 ]; then
+    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$BASTION_IP" ubuntu@$TARGET_IP "$@"
+  else
+    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$BASTION_IP" ubuntu@$TARGET_IP
+  fi
+
+# Case 4: Bad usage
+else
   echo "Please provide bastion IP address"
   exit 5
 fi
-
-if [ -z "$2" ]; then
-  echo "Please provide private IP of the target instance"
-  exit 5
-fi
-
-if [ -z "$3" ]; then
-  echo "Please provide a command to run on the target"
-  exit 5
-fi
-
-
-BASTION_IP="$1"
-TARGET_PRIVATE_IP="$2"
-shift 2
-COMMAND="$@"
-
-
-ssh -i "$KEY_PATH" \
-  -o StrictHostKeyChecking=no \
-  -o ProxyCommand="ssh -i $KEY_PATH -o StrictHostKeyChecking=no -W %h:%p ubuntu@$BASTION_IP" \
-  ubuntu@$TARGET_PRIVATE_IP "$COMMAND"
