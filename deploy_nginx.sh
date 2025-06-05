789e586cc506b15f3ed1d45dc3e250878c303195
Aseel <aseelarr@gmail.com>
2025-06-05 09:51:21 +0000

    update the files

---
+++ deploy_nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Stopping and removing old Nginx container (if any)..."
+docker stop mynginx || true
+docker rm mynginx || true
+
+echo "Starting Nginx container..."
+docker run -d --name mynginx -p 443:443 \
+  -v /home/ubuntu/conf.d:/etc/nginx/conf.d/ \
+  -v /home/ubuntu/certs:/etc/ssl \
+  nginx
